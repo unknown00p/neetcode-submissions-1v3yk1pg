b49e06e06b974a45fcb29ae67dc7900d261106c1
Bot <rajpoint8976@gmail.com>
2026-05-14 12:30:39 +0530

    Add: duplicate-integer - submission-5

---
+++ Data Structures & Algorithms/duplicate-integer/submission-5.js
@@ -0,0 +1,40 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {boolean}
+     */
+    hasDuplicate(nums) {
+        // for(let i=0; i < nums.length; i++){
+        //     for (let j = i + 1; j < nums.length; j++){
+        //         if(nums[i] === nums[j]){
+        //             return true
+        //         }
+        //     }
+        // }
+
+
+        let uniqueArr = new Set()
+        for (let num of nums) {
+            uniqueArr.add(num);
+        }
+
+        if(uniqueArr.size !== nums.length){
+            return true
+        }
+
+        return false
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
